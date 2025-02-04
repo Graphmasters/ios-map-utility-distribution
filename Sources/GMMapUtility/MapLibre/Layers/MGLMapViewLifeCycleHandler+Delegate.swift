a30039033bbf1b5feafc95c3ef03831da75a0191
Florian herzog <florian.herzog@graphmasters.net>
2025-02-04 16:00:09 +0100

    :bookmark: 1.3.0 [ci skip]

--- Sources/GMMapUtility/MapLibre/Layers/MGLMapViewLifeCycleHandler+Delegate.swift
+++ Sources/GMMapUtility/MapLibre/Layers/MGLMapViewLifeCycleHandler+Delegate.swift
@@ -1,7 +1,11 @@
 import Mapbox
 
 public protocol MGLMapViewLifeCycleHandlerDelegate: AnyObject {
-    func mapView(_ mapView: MGLMapView, didFinishLoading style: MGLStyle, handledBy handler: MGLMapViewLifeCycleHandler)
+    func mapView(
+        _ mapView: MGLMapView,
+        didFinishLoading style: MGLStyle,
+        handledBy handler: MGLMapViewLifeCycleHandler
+    )
 
     func mapView(
         _ mapView: MGLMapView,
@@ -22,4 +26,32 @@ public protocol MGLMapViewLifeCycleHandlerDelegate: AnyObject {
         animated: Bool,
         handledBy handler: MGLMapViewLifeCycleHandler
     )
+}
+
+extension MGLMapViewLifeCycleHandlerDelegate {
+    public func mapView(
+        _: MGLMapView,
+        didFinishLoading _: MGLStyle,
+        handledBy _: MGLMapViewLifeCycleHandler
+    ) {}
+
+    public func mapView(
+        _: MGLMapView,
+        regionWillChangeWith _: MGLCameraChangeReason,
+        animated _: Bool,
+        handledBy _: MGLMapViewLifeCycleHandler
+    ) {}
+
+    public func mapView(
+        _: MGLMapView,
+        regionIsChangingWith _: MGLCameraChangeReason,
+        handledBy _: MGLMapViewLifeCycleHandler
+    ) {}
+
+    public func mapView(
+        _: MGLMapView,
+        regionDidChangeWith _: MGLCameraChangeReason,
+        animated _: Bool,
+        handledBy _: MGLMapViewLifeCycleHandler
+    ) {}
 }
